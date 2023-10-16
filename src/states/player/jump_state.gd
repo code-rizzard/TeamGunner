6f38a03138341f3dbad233269215402b0752df02
Richard <richard232manansala@gmail.com>
2023-10-16 12:19:03 +0800

    Made jumping higher by holding jump button - removed crouch jump

--- src/states/player/jump_state.gd
+++ src/states/player/jump_state.gd
@@ -6,7 +6,15 @@ func  _on_enter(prev_state : State) -> void:
 	super(prev_state)
 	self.parent.anim_player.play("jump")
 	parent.velocity.y = -parent.jump_velocity
-	if prev_state == crouch_state:
-		parent.velocity.y = -sqrt(2*parent.gravity*(parent.jump_height + 0.6) * 32)
+
+func _on_unhandled_input(e : InputEvent) -> State:
+	var r := super(e)
+	if r != null:
+		return r
+	if e.is_action_released("jump"):
+		parent.velocity.y = max(-100, parent.velocity.y)
+		return fall_state
+	return null
+
 
 
