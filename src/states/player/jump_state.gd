94cb1f79ff56e9b390e5c63931916dfde13b5226
Richard <richard232manansala@gmail.com>
2023-10-16 10:11:44 +0800

    Added crouch jump

--- src/states/player/jump_state.gd
+++ src/states/player/jump_state.gd
@@ -6,5 +6,7 @@ func  _on_enter(prev_state : State) -> void:
 	super(prev_state)
 	self.parent.anim_player.play("jump")
 	parent.velocity.y = -parent.jump_velocity
+	if prev_state == crouch_state:
+		parent.velocity.y = -sqrt(2*parent.gravity*(parent.jump_height + 0.6) * 32)
 
 
