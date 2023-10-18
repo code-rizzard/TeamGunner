325fe8e60206e3290187c42eeebc7c62c71fb185
Richard <richard232manansala@gmail.com>
2023-10-18 17:40:20 +0800

    red enemy can now ignore target if out of reach

--- src/mobs/enemies/red/states/shoot_state.gd
+++ src/mobs/enemies/red/states/shoot_state.gd
@@ -3,9 +3,15 @@ extends "res://src/mobs/enemies/red/states/base_state.gd"
 @export var idle_state : State
 var can_shoot := true
 
+var too_far_timer : SceneTreeTimer
+
 func  _on_enter(prev_state : State) -> void:
 	super(prev_state)
 	self.parent.anim_player.play("attack")
+
+func _on_exit(new : State) -> void:
+	super(new)
+	too_far_timer = null
 
 func _on_physics_process(delta : float) -> State:
 	var r := super(delta)
@@ -14,15 +20,27 @@ func _on_physics_process(delta : float) -> State:
 	if parent.target == null:
 		return idle_state
 	var dist : Vector2 = abs(parent.target.global_position - parent.global_position)
-	if (dist.y >=32 * 3 and dist.x > 32 * 2) or dist.x >= 5 * 32.5:
+
+	if dist.x >= 5 * 32.5:
 		parent.target = null
 		return idle_state
 	
+
+	if (dist.y >=32 * 3 or dist.x > 32 * 2) :
+	
+		if too_far_timer == null:
+			too_far_timer = get_tree().create_timer(2.5)
+		elif too_far_timer.time_left == 0:
+			if (dist.y >=32 * 2 and dist.x > 32 * 2) :
+				parent.target = null
+				return idle_state
+			too_far_timer = null
+		
+
+
 	if can_shoot and dist.y < 32:
 		shoot()
 	return null
-
-
 
 
 func shoot():
