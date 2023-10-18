4f098455268b32b83b3633045dda2b9c65dab7ec
Richard <richard232manansala@gmail.com>
2023-10-18 17:42:25 +0800

    set 5.5 tiles * 32 px instead of other way around

--- src/mobs/enemies/red/states/shoot_state.gd
+++ src/mobs/enemies/red/states/shoot_state.gd
@@ -21,7 +21,7 @@ func _on_physics_process(delta : float) -> State:
 		return idle_state
 	var dist : Vector2 = abs(parent.target.global_position - parent.global_position)
 
-	if dist.x >= 5 * 32.5:
+	if dist.x >= 5.5 * 32:
 		parent.target = null
 		return idle_state
 	
