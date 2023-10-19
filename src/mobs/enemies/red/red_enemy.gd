80b0cba52fa920dc62dda5304e7b801e675b2066
Richard <richard232manansala@gmail.com>
2023-10-19 19:54:14 +0800

    Fixed the bullet not spawning at muzzle

--- src/mobs/enemies/red/red_enemy.gd
+++ src/mobs/enemies/red/red_enemy.gd
@@ -1,13 +1,6 @@
 extends NpcMob
 
 
-const muzzle_positions := {
-	"idle": Vector2(9,-3),
-	"walk": Vector2(9,-3),
-	"jump": Vector2(10,-3),
-	"fall": Vector2(10,-3),
-	"crouch": Vector2(10,1),
-}
 
 @onready var space := get_world_2d().direct_space_state
 
