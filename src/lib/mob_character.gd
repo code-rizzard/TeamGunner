693b73d90bb69de2ac051bc7347d3dacbf393a5d
Richard <richard232manansala@gmail.com>
2023-10-19 11:48:04 +0800

    Added attack_cd

--- src/lib/mob_character.gd
+++ src/lib/mob_character.gd
@@ -7,11 +7,11 @@ signal on_die
 @export var max_health := 100
 @export var movement_speed = 100.0
 @export var jump_height := 2.0
+@export var attack_cd := 0.5
 @export_flags_2d_physics var attack_mask := 1
 @export var hurtbox : Hurtbox
 @export var anim_player : AnimatedSprite2D
 @export var muzzle : Marker2D
-
 
 var look_direction : 
 	get: return -1 if anim_player.flip_h else 1
--- src/mobs/player/Player.gd
+++ src/mobs/player/Player.gd
@@ -2,7 +2,7 @@ extends MobCharacter
 class_name Player
 
 
-
+var can_shoot := true
 
 const muzzle_positions := {
 	"idle": Vector2(9,-3),
@@ -22,11 +22,14 @@ var input_direction : float
 
 
 func _input(_event):
-	if Input.is_action_just_pressed("shoot"):
+	if Input.is_action_just_pressed("shoot") and can_shoot:
 		shoot()	
 
 func shoot():
+	can_shoot = false
 	muzzle.shoot(-1 if anim_player.flip_h else 1)
+	await get_tree().create_timer(attack_cd).timeout
+	can_shoot = true
 
 
 
