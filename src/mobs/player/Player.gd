9bc58715498e194a361ecbf82ba10c4221ae71bb
Richard <richard232manansala@gmail.com>
2023-10-19 18:14:38 +0800

    added ammo count to player

--- src/mobs/player/Player.gd
+++ src/mobs/player/Player.gd
@@ -2,8 +2,11 @@ extends MobCharacter
 class_name Player
 
 
-var can_shoot := true
+@export var max_ammo := 50
 
+@onready var current_ammo := max_ammo
+
+var can_shoot := true
 const muzzle_positions := {
 	"idle": Vector2(9,-3),
 	"walk": Vector2(9,-3),
@@ -22,12 +25,13 @@ var input_direction : float
 
 
 func _input(_event):
-	if Input.is_action_just_pressed("shoot") and can_shoot:
+	if Input.is_action_just_pressed("shoot") and can_shoot and current_ammo >0:
 		shoot()	
 
 func shoot():
 	can_shoot = false
 	muzzle.shoot(-1 if anim_player.flip_h else 1)
+	current_ammo -= 1
 	await get_tree().create_timer(attack_cd).timeout
 	can_shoot = true
 
